92557468e9c66dda3f8966db35306e92238548b1
Sammers21 <titantins@gmail.com>
2023-04-23 20:18:00 +0300

    releaseb fixing main

--- src/io.github.sammers.pla/Main.java
+++ src/io.github.sammers.pla/Main.java
@@ -1,6 +1,8 @@
 package io.github.sammers.pla;
 
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.mongo.MongoClient;
 import io.vertx.reactivex.ext.web.client.WebClient;
 import org.ocpsoft.prettytime.PrettyTime;
 
@@ -19,18 +21,15 @@ public class Main {
         final String dbUri = System.getenv("DB_URI");
         final String clientId = System.getenv("CLIENT_ID");
         final String clientSecret = System.getenv("CLIENT_SECRET");
-//        final MongoClient mongoClient = MongoClient.createShared(vertx, new JsonObject()
-//            .put("db_name", "pvpq")
-//            .put("connection_string", dbUri)
-//            .put("maxPoolSize", 3)
-//        );
+        final MongoClient mongoClient = MongoClient.createShared(vertx, new JsonObject()
+            .put("db_name", "pvpq")
+            .put("connection_string", dbUri)
+            .put("maxPoolSize", 3)
+        );
         final BlizzardAPI blizzardAPI = new BlizzardAPI(clientId, clientSecret, webClient);
-//        DB db = new DB(mongoClient);
-        blizzardAPI.cutoffs("eu").subscribe(cutoffs -> {
-            System.out.println(cutoffs.toJson());
-        });
-//        Ladder ladder = new Ladder(vertx, webClient, db, blizzardAPI);
-//        ladder.start();
-//        new Http(vertx, ladder).start();
+        DB db = new DB(mongoClient);
+        Ladder ladder = new Ladder(vertx, webClient, db, blizzardAPI);
+        ladder.start();
+        new Http(vertx, ladder).start();
     }
 }
