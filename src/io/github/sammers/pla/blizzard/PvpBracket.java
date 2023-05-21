60e412a33d62b7cf51753883d4ad44a03ad47ee9
Sammers21 <titantins@gmail.com>
2023-05-21 14:52:31 +0300

    releaseb use 0 if won or lost is null

--- src/io/github/sammers/pla/blizzard/PvpBracket.java
+++ src/io/github/sammers/pla/blizzard/PvpBracket.java
@@ -3,19 +3,21 @@ package io.github.sammers.pla.blizzard;
 import io.github.sammers.pla.http.JsonConvertable;
 import io.vertx.core.json.JsonObject;
 
+import java.util.Optional;
+
 public record PvpBracket(String bracketType, Long rating, Long won, Long lost) implements JsonConvertable {
     public static PvpBracket parse(JsonObject entries) {
         String type = entries.getJsonObject("bracket").getString("type");
         Long rating = entries.getLong("rating");
-        JsonObject stats;
+        Optional<JsonObject> stats;
         if (type.equals("SHUFFLE")) {
             type = type + "-" + entries.getJsonObject("specialization").getString("name");
-            stats = entries.getJsonObject("season_round_statistics");
+            stats = Optional.ofNullable(entries.getJsonObject("season_round_statistics"));
         } else {
-            stats = entries.getJsonObject("season_match_statistics");
+            stats = Optional.ofNullable(entries.getJsonObject("season_match_statistics"));
         }
-        Long won = stats.getLong("won");
-        Long lost = stats.getLong("lost");
+        Long won = stats.map(st -> st.getLong("won")).orElse(0L);
+        Long lost = stats.map(st -> st.getLong("lost")).orElse(0L);
         return new PvpBracket(type, rating, won, lost);
     }
 
