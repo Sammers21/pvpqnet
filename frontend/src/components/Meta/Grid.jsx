3e0a520e727b3a23f93e4c2911810efbcad42488
Sammers21 <titantins@gmail.com>
2023-06-19 00:23:00 +0300

    add grid dependency

---
+++ frontend/src/components/Meta/Grid.jsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import {containerBg} from "../../theme";
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+
+const Grid = () => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        backgroundColor: containerBg,
+        minHeight: '100vh',
+        paddingTop: '105px',
+        paddingLeft: '3%',
+        paddingRight: '3%',
+        paddingBottom: '45px',
+    }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
+
+export default Grid;
--- frontend/src/components/Meta/index.js
+++ frontend/src/components/Meta/index.js
@@ -1,10 +1,26 @@
 import PageHeader from "../AppBar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Footer from "../Footer";
+import {useParams} from "react-router-dom";
+import {capitalizeFirstLetter} from "../../containers/Activity";
+import axios from "axios";
+import {baseUrl} from "../../config";
+import Grid from "./Grid";
 
 const Meta = ({}) => {
+  let [data, setData] = useState({});
+  document.title = `Meta`;
+  const loadMeta = async () => {
+    const data = (await axios.get(baseUrl + `/api/meta`)).data
+    setData(data);
+    console.log("data", data);
+  };
+  useEffect(() => {
+    loadMeta();
+  }, []);
   return (<>
     <PageHeader/>
+    <Grid data={data}></Grid>
     <Footer/>
   </>);
 }
