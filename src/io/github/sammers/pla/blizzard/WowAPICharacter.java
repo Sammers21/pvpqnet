55396d44448ad852a777ad4a59ccc2cebf8532e3
Sammers21 <titantins@gmail.com>
2023-06-29 16:54:33 +0300

    releaseb going 25x faster

--- src/io/github/sammers/pla/blizzard/WowAPICharacter.java
+++ src/io/github/sammers/pla/blizzard/WowAPICharacter.java
@@ -161,7 +161,7 @@ public record WowAPICharacter(long id, String name, String realm, String gender,
             .collect(Collectors.toSet());
         Long lastUpdatedUTCms = Instant.now().toEpochMilli();
         String name = entries.getString("name").substring(0, 1).toUpperCase() + entries.getString("name").substring(1);
-1        String realm = entries.getJsonObject("realm").getString("name").substring(0, 1).toUpperCase() +
+        String realm = entries.getJsonObject("realm").getString("name").substring(0, 1).toUpperCase() +
                 entries.getJsonObject("realm").getString("name").substring(1);
         return new WowAPICharacter(
             entries.getInteger("id"),
