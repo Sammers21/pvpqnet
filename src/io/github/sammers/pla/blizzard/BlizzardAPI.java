8d7fb8f2d4b497250e1e6fb6b18ad1e95fb2d1ee
Sammers21 <titantins@gmail.com>
2023-07-03 03:26:26 +0300

    releaseb going 5 per sec

--- src/io/github/sammers/pla/blizzard/BlizzardAPI.java
+++ src/io/github/sammers/pla/blizzard/BlizzardAPI.java
@@ -30,7 +30,7 @@ public class BlizzardAPI {
     private final WebClient webClient;
     private final String clientId;
     private final AtomicReference<BlizzardAuthToken> token = new AtomicReference<>();
-    private final RateLimiter rateLimiter = new RateLimiter(20, Main.VTHREAD_SCHEDULER);
+    private final RateLimiter rateLimiter = new RateLimiter(5, Main.VTHREAD_SCHEDULER);
 
     public BlizzardAPI(String clientId, String clientSecret, WebClient webClient) {
         this.clientId = clientId;
