74c949d5535fcbe33f7184d7b9b2b59d4640e5d4
Sammers21 <titantins@gmail.com>
2023-09-24 15:44:51 +0300

    add timestamp indexes on every collection + profile region index

--- pg.mongodb.js
+++ pg.mongodb.js
@@ -1,3 +1,7 @@
 // Select the database to use.
 use('pvpq');
 db.profile.createIndex( { region: -1 } )
+db.getCollection('3v3').createIndex({timestamp: -1})
+db.getCollection('2v2').createIndex({timestamp: -1})
+db.battlegrounds.createIndex({timestamp: -1})
+db.shuffle.createIndex({timestamp: -1})
