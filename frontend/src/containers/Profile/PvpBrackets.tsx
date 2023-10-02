f32e51d7d62139cb7be6642b56e687754a2c1302
Roman Borovik <starmordar@gmail.com>
2023-10-02 16:59:12 +0300

    Redesign cards

--- frontend/src/containers/Profile/PvpBrackets.tsx
+++ frontend/src/containers/Profile/PvpBrackets.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
-import { Chip } from '@mui/material';
+import { Chip, LinearProgress } from '@mui/material';
 
-import { getSpecIcon, ratingToColor } from '../../utils/table';
+import { getSpecIcon, getWonAndLossColors, ratingToColor } from '../../utils/table';
 import { CLASS_AND_SPECS } from '../../constants/filterSchema';
 import type { IPlayerBracket, IPlayer } from '../../types';
 
@@ -41,86 +41,104 @@ const PvpBracket = ({
     const won = bracket?.won || 0;
     const lost = bracket?.lost || 0;
 
-    const wonColor = won > 0 ? 'green' : '#ffffff';
+    const { wonColor, lossColor } = getWonAndLossColors(won, lost);
     const showWinRate = won > 0 || lost > 0;
     const winRate = ((won * 100) / (won + lost)).toFixed(2);
     const winRateColor = parseInt(winRate, 10) >= 50 ? 'green' : '#ff0531';
 
-    return { showWinRate, winRateColor, wonColor, winRate };
+    return { showWinRate, winRateColor, wonColor, lossColor, winRate };
   }, [bracket]);
 
   return (
-    <div className="relative flex grow flex-col items-center border border-solid rounded-lg border-[#37415180] px-1 md:px-3 py-2 bg-[#030303e6]">
-      <div className="flex justify-center items-center mx-2">
-        <span
-          className="flex flex-col md:flex-row items-center gap-0 md:gap-2 pb-1 text-xl md:text-4xl font-semibold"
-          style={{ color: ratingColor }}
-        >
-          {specIcon ? (
-            <img
-              className="h-8 w-8 rounded border border-solid border-[#37415180]"
-              src={specIcon}
-              alt="spec icon"
-            />
-          ) : (
-            <span className="text-base md:text-2xl text-white md:mt-1">{title}</span>
-          )}
+    <div className="relative flex w-1/3 flex-col items-center px-1">
+      <div className="flex flex-col w-full rounded-lg border pb-2 border-solid border-[#37415180] bg-[#030303e6]">
+        <div className="relative flex flex-col w-full justify-start gap-0 px-3 py-2">
+          <div className="flex w-full justify-between">
+            {specIcon ? (
+              <div className="flex items-center gap-2 my-2">
+                <img
+                  className="h-8 w-8 rounded border border-solid border-[#37415180]"
+                  src={specIcon}
+                  alt="spec icon"
+                />
+              </div>
+            ) : (
+              <span className="text-base md:text-2xl text-white">{title}</span>
+            )}
+            {bracket?.max_rating && bracket.max_rating !== -1 ? (
+              <div className="flex gap-2 flex-row justify-center items-center text-[#60A5FACC] text-sm">
+                <span className="text-sm">Highest</span>
+                <span className="text-white text-xs md:text-lg">{bracket.max_rating}</span>
+              </div>
+            ) : null}
+          </div>
 
-          <div className="flex gap-1 md:gap-2">
-            {bracket?.rating || 0}
-
+          <div className="flex gap-2 mt-1 pt-1 border-t border-solid border-[#37415180]">
+            <span
+              className="flex flex-col md:flex-row items-center gap-0 md:gap-2 text-xl md:text-4xl font-semibold"
+              style={{ color: ratingColor }}
+            >
+              {bracket?.rating || 0}
+            </span>
             {bracket?.rank && bracket.rank !== -1 ? (
               <div className="flex items-center justify-between absolute top-1 right-0 md:relative">
                 <Chip className="!text-[8px] md:!text-sm" label={`#${bracket.rank}`} size="small" />
               </div>
             ) : null}
           </div>
-        </span>
-      </div>
-
-      <div className="pt-1 flex flex-col w-full border-t border-solid border-[#60A5FA50]">
-        <div className="pb-1 flex md:flex-row flex-col text-[#60A5FACC] text-sm">
-          <div className="flex grow">
-            <div className="flex flex-col items-center grow border-r border-solid border-[#60A5FA50] px-1">
-              <span className="md:flex hidden">total</span>
-              <span className="text-sm md:text-base text-white">
-                {(bracket?.lost || 0) + (bracket?.won || 0)}
-              </span>
-            </div>
-
-            <div className="flex flex-col items-center grow px-1">
-              <span className="md:flex hidden">wins</span>
-              <span className="text-sm md:text-base" style={{ color: stats.wonColor }}>
-                {bracket?.won || 0}
-              </span>
-            </div>
-          </div>
-
-          {stats.showWinRate && (
-            <div className="flex flex-col items-center grow px-1 md:border-l border-solid border-[#60A5FA50]">
-              <span className="md:flex hidden">winrate</span>
-              <span style={{ color: stats.winRateColor }} className="text-sm md:text-base">
-                {stats.winRate}%
-              </span>
-            </div>
-          )}
         </div>
 
-        <div className="flex flex-col md:flex-row md:gap-4 text-[#60A5FACC] md:pt-1 text-sm border-t border-solid border-[#60A5FA50]">
+        <div className="px-4 pb-2">
+          <div className="pb-1 gap-2 flex md:flex-row flex-col text-[#60A5FACC] text-sm">
+            <div className="flex">
+              <span className="text-sm md:text-base text-white">
+                {bracket?.won || 0}
+                <span className="ml-[1px] text-[#008000]">W</span>
+              </span>
+
+              <span className="text-sm md:text-base text-white pl-2">
+                {bracket?.lost || 0}
+                <span className="ml-[1px] text-[#ff0000]">L</span>
+              </span>
+            </div>
+
+            {stats.showWinRate && (
+              <div className="flex pl-2 md:border-l border-solid border-[#37415180]">
+                <span style={{ color: stats.winRateColor }} className="text-sm md:text-base">
+                  {stats.winRate}%
+                </span>
+              </div>
+            )}
+          </div>
+          <LinearProgress
+            variant="determinate"
+            value={parseInt(stats.winRate, 10)}
+            sx={{
+              '& .MuiLinearProgress-bar': { backgroundColor: '#008000' },
+              '&.MuiLinearProgress-root': { backgroundColor: '#ff0000' },
+            }}
+          />
+        </div>
+
+        {/* <div className="flex w-full flex-col md:flex-row md:gap-1 text-[#60A5FACC] text-sm border-t border-solid border-[#37415180]">
           {bracket?.season_max_rating && bracket.season_max_rating !== -1 ? (
-            <div className="px-1 md:py-1 gap-1 grow flex justify-center items-center border-t md:border-t-0 md:border-r border-solid border-[#60A5FA50]">
-              <span>Season</span>
-              <span className="text-white text-xs md:text-base">{bracket.season_max_rating}</span>
+            <div className="px-1 md:py-1 w-1/2 flex flex-col justify-center items-center">
+              <span className="text-xs">Season</span>
+              <span className="text-white font-semibold text-xs md:text-xl">
+                {bracket.season_max_rating}
+              </span>
             </div>
           ) : null}
 
           {bracket?.max_rating && bracket.max_rating !== -1 ? (
-            <div className="px-1 md:py-1 gap-1 grow flex justify-center items-center">
-              <span>Record</span>
-              <span className="text-white text-xs md:text-base">{bracket.max_rating}</span>
+            <div className="px-1 md:py-1 w-1/2 flex flex-col justify-center items-center">
+              <span className="text-xs">Record</span>
+              <span className="text-white font-semibold text-xs md:text-xl">
+                {bracket.max_rating}
+              </span>
             </div>
           ) : null}
-        </div>
+        </div> */}
       </div>
     </div>
   );
@@ -132,14 +150,14 @@ const PvpBrackets = ({ player }: IProps) => {
 
   return (
     <div className="flex gap-2 justify-start flex-col">
-      <div className="flex flex-nowrap gap-2 justify-start">
+      <div className="flex flex-nowrap justify-start">
         {arenaAndRbg.map(({ title, name }) => {
           const playerBracket = (player?.brackets || []).find((b) => b.bracket_type === name);
           return <PvpBracket key={name} title={title} bracket={playerBracket} />;
         })}
       </div>
 
-      <div className="flex flex-wrap gap-2 justify-start">
+      <div className="flex flex-wrap justify-start">
         {classAndSpec.map((spec) => {
           const playerBracket = (player?.brackets || []).find((b) => b.bracket_type.includes(spec));
           return (
