4e4c11c7ea96117faca603c49afdcda26dbd325f
Roman Borovik <starmordar@gmail.com>
2023-11-24 19:18:56 +0300

    Perlace footer icons

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,30 +1,27 @@
-import Link from '@mui/material/Link';
-
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { GitHubIcon, DiscordIcon } from '@/components/icons';
+import ExternalLink from '@/components/common/ExternalLink';
 
 const Footer = () => {
   return (
     <div className="flex justify-between flex-wrap py-1 px-4 md:px-8 fixed bottom-0 left-0 bg-zinc-950 w-full border-solid border-t border-[#2f384de6]">
       <div className="pr-4">
-        Made by<span> </span>
-        <Link underline="none" href="https://github.com/Sammers21">
+        Made by&nbsp;
+        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Sammers21">
           Sammers
-        </Link>
-        <span> </span>&<span> </span>
-        <Link underline="none" href="https://github.com/Starmordar">
+        </ExternalLink>
+        &nbsp;&&nbsp;
+        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Starmordar">
           Starmordar
-        </Link>
+        </ExternalLink>
       </div>
 
       <div className="flex items-center gap-4">
-        <Link color="#FFFFFF" underline="none" href="https://github.com/Sammers21/wow-pla/issues">
+        <ExternalLink href="https://github.com/Sammers21/wow-pla/issues">
           <GitHubIcon />
-        </Link>
-        <Link color="#FFFFFF" href="https://discord.com/users/343752113752506379">
-          <FontAwesomeIcon icon={faDiscord} />
-        </Link>
+        </ExternalLink>
+        <ExternalLink href="https://discord.com/users/343752113752506379">
+          <DiscordIcon />
+        </ExternalLink>
       </div>
     </div>
   );
