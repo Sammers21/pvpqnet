b235899d85ad7f2b2c387fdc5613df50a0c94bc5
Sammers21 <titantins@gmail.com>
2023-11-27 01:42:03 +0300

    releasef footer improvements

--- frontend/src/components/ExternalLink.tsx
+++ frontend/src/components/ExternalLink.tsx
@@ -5,15 +5,17 @@ interface IProps {
   href: string;
   children: React.ReactNode;
   className?: string;
+  color?: string;
 }
 
-const ExternalLink = ({ href, children, className }: IProps) => {
+const ExternalLink = ({ href, children, className, color }: IProps) => {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
       className={cn('no-underline', className)}
+      style={{ color: color }}
     >
       {children}
     </a>
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,16 +1,23 @@
 import { GitHubIcon, DiscordIcon } from '@/components/IIcons';
 import ExternalLink from '@/components/ExternalLink';
+import { getClassIcon, getClassNameColor } from '@/utils/table';
 
 const Footer = () => {
+  const priest = getClassIcon('PRIEST');
+  const priestColor = getClassNameColor('PRIEST');
+  const deathKnight = getClassIcon('DEATH KNIGHT');
+  const deathKnightColor = getClassNameColor('DEATH KNIGHT');
   return (
     <div className="flex justify-between flex-wrap py-1 px-4 md:px-8 fixed bottom-0 left-0 bg-zinc-950 w-full border-solid border-t border-[#2f384de6]">
       <div className="pr-4">
         Made by&nbsp;
-        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Sammers21">
+        <img className="inline-block w-6 h-6 mx-1" src={priest} alt="priest" />
+        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Sammers21" color={priestColor} >
           Sammers
         </ExternalLink>
-        &nbsp;&&nbsp;
-        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Starmordar">
+        &nbsp; & &nbsp;
+        <img className="inline-block w-6 h-6 mx-1" src={deathKnight} alt="death knight" />
+        <ExternalLink className="text-[#3f50b5]" href="https://github.com/Starmordar" color={deathKnightColor}>
           Starmordar
         </ExternalLink>
       </div>
