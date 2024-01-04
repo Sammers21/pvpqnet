8d7a4ddd91b99543ca28c256b142f3f671cd2968
Sammers21 <titantins@gmail.com>
2024-01-04 21:35:05 +0300

    create profile id index

--- db-schema.mongodb.js
+++ db-schema.mongodb.js
@@ -1,6 +1,7 @@
 // Select the database to use.
 use('pvpq');
 db.profile.createIndex( { region: -1 } )
+db.profile.createIndex( { id: -1 } )
 db.getCollection('3v3').createIndex({timestamp: -1})
 db.getCollection('2v2').createIndex({timestamp: -1})
 db.battlegrounds.createIndex({timestamp: -1})
