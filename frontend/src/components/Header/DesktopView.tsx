3ead67495f8486adaf8cd5760ead07c9ab770ea8
Sammers21 <titantins@gmail.com>
2024-07-05 04:42:53 +0300

    releasef fix the search bar position

--- frontend/src/components/Header/DesktopView.tsx
+++ frontend/src/components/Header/DesktopView.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@mui/material';
-import PlayersSearch from '@/components/SearchBar';
-import RegionButtons from './RegionButtons';
+import { Button } from "@mui/material";
+import PlayersSearch from "@/components/SearchBar";
+import RegionButtons from "./RegionButtons";
 
-import { REGION } from '@/constants/region';
+import { REGION } from "@/constants/region";
 
 interface IProps {
   host: string;
@@ -14,26 +14,29 @@ interface IProps {
 const DesktopView = ({ menuItems, host, region, setRegion }: IProps) => {
   return (
     <div className="flex items-center justify-between w-full">
-      <div className="flex items-center justify-start grow pr-6">
+      <div className="flex items-center justify-start">
         <div className="flex items-center justify-start">
-          <a href="/" className="sm:flex hidden ml-20 mr-5 font-bold tracking-wider text-xl">
+          <a
+            href="/"
+            className="sm:flex hidden ml-20 mr-5 font-bold tracking-wider text-xl"
+          >
             {host}
           </a>
 
           {menuItems.map((item) => (
-            <Button key={item.label} className="!text-[#60a5fa]" onClick={item.onClick}>
+            <Button
+              key={item.label}
+              className="!text-[#60a5fa]"
+              onClick={item.onClick}
+            >
               {item.label}
             </Button>
           ))}
         </div>
-
-        <div className="flex grow items-center justify-start ml-10 xl:ml-[8rem]">
-          <div className="flex items-center justify-end basis-[25rem]">
-            <PlayersSearch />
-          </div>
-        </div>
       </div>
-
+      <div className="flex items-center w-[356px]">
+          <PlayersSearch />
+      </div>
       <div className="flex items-center justify-end mr-20">
         <RegionButtons region={region} setRegion={setRegion} />
       </div>
