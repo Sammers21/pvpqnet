1fb5c9bc3ef0df318c2084ca6fe4b119f0bff685
Sammers21 <titantins@gmail.com>
2024-09-11 08:17:04 +0300

    new search

--- src/io/github/sammers/pla/logic/SearchResult.java
+++ src/io/github/sammers/pla/logic/SearchResult.java
@@ -3,7 +3,22 @@ package io.github.sammers.pla.logic;
 import io.github.sammers.pla.http.JsonConvertable;
 import io.vertx.core.json.JsonObject;
 
+import java.util.Objects;
+
 public record SearchResult(String nick, String region, String clazz) implements JsonConvertable {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SearchResult that = (SearchResult) o;
+        return Objects.equals(nick, that.nick);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nick);
+    }
 
     @Override
     public JsonObject toJson() {
