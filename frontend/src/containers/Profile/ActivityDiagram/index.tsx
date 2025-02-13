9b7a040b982aa53fb8395e07bd8780d2fe4fd28f
Medivero <149628365+Medivero@users.noreply.github.com>
2025-02-13 03:06:27 +0300

    Add buttons changing the year on the Activity diagram
    * Added buttons to change years at activity diagram in Profile page
    
    * Fixed years buttons
    
    * One more fix years
    
    * Added activity in the last year
    
    * Fixed saving selected year after switch character page
    
    * Changed positon of year buttons and added styles for them
    
    * Fixed actually year display at activity diagram
    
    * Changed position of buttons and scale of activity diagram
    
    * Fixed dates at activity diagram
    
    * Few changes
    
    * Optimizated activity diagram

--- frontend/src/containers/Profile/ActivityDiagram/index.tsx
+++ frontend/src/containers/Profile/ActivityDiagram/index.tsx
@@ -10,7 +10,7 @@ import {
   styled,
 } from "@mui/material";
 import { ActivityBox } from "./ActivityBox";
-
+import { useState, useEffect } from "react";
 interface IProps {
   player: Player;
   year?: number;
@@ -22,9 +22,6 @@ const StyledTableCell = styled(TableCell)({
   width: "10px",
   height: "10px",
 });
-
-const currentYear = new Date().getFullYear();
-
 function activityHistoryForPlayer(player: Player) {
   let totalPlayers = [player, ...player.alts];
   return totalPlayers
@@ -35,7 +32,7 @@ function activityHistoryForPlayer(player: Player) {
 }
 function getDayOfWeekRender(numberOfDay) {
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  if (numberOfDay % 2 == 0) {
+  if (numberOfDay % 2 === 0) {
     return (
       <StyledTableCell component="th" scope="row" size="small" align="left">
         {" "}
@@ -53,24 +50,34 @@ export function gamesPlayedByActivityArray(activityArray) {
     .reduce((a, b) => a + b, 0);
 }
 
-const ActivityDiagram = ({ player, year = currentYear }: IProps) => {
-  if (false) {
-    return <> </>;
-  } else {
+const currentDate = new Date().toString()
+const ActivityDiagram = ({player}: IProps) => {
+  const [selectedYear,setSelectedYear ] = useState(currentDate);
+  useEffect(() => {
+    setSelectedYear(currentDate)
+  },[player])
+    let fullHistory = activityHistoryForPlayer(player);
     let weeks = [];
     var currentWeek = [];
-    let fullHistory = activityHistoryForPlayer(player);
     let dateAndActivity = {};
-    let start = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-    let end = new Date();
-    let currentMonth = start.getMonth();
+    let start;
+    let end;
+    let currentMonth;
+    if (selectedYear !== currentDate){
+      start = new Date(Number(selectedYear),0,0,0,0,0);
+      end = new Date(new Date(Number(selectedYear),11,30).toString());
+    }
+    else{
+      start = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+      end = new Date()
+      currentMonth = start.getMonth();
+    }
     var loop = new Date(start);
     while (loop <= end) {
       let dt = loop.toLocaleDateString();
       let dayOfWeek = loop.getDay();
       currentWeek[dayOfWeek] = loop;
-      // currentWeek.push(loop);
-      if (dayOfWeek == 6) {
+      if (dayOfWeek === 6) {
         weeks.push(currentWeek);
         currentWeek = [];
       }
@@ -134,95 +141,138 @@ const ActivityDiagram = ({ player, year = currentYear }: IProps) => {
         week.find((d) => d != null).date.getMonth()
       ].weekCount += 1;
     });
-    let monthNumbers = Array.from(Array(12).keys());
+    let monthNumbers = Array.from(Array(12).keys())
     let totalGamesPlayed = fullHistory
       .filter((activity) => {
-        return new Date(activity.diff.timestamp) > start;
+        return (new Date(activity.diff.timestamp) > start && new Date(activity.diff.timestamp) < end);
       })
-      .map((activity) => {
-        return activity.diff.won + activity.diff.lost;
-      })
-      .reduce((a, b) => a + b, 0);
+      .reduce((sum, activity) => sum + activity.diff.won + activity.diff.lost, 0);
+    const years = [];
+    fullHistory.forEach((item) => {
+      const yearButtonValue = new Date(item.diff.timestamp).getFullYear().toString();
+      if (!years.includes(yearButtonValue)) {
+        return years.push(yearButtonValue);
+      }
+    });
+    years.forEach((item) => {
+      let GamesAtSelectedYear = fullHistory.filter(activity => item === new Date(activity.diff.timestamp).getFullYear().toString())
+      .reduce((sum,activity) => sum + activity.diff.won + activity.diff.lost,0);
+      return GamesAtSelectedYear>0;
+    })
+    years.sort((a,b) => b-a);
     return (
-      <div className="flex flex-col py-2 md:px-3 border border-solid border-[#37415180] rounded-lg bg-[#030303e6] ">
-        <span className="text-2xl mr-4">
-          {totalGamesPlayed} games played in the last year
-        </span>
-        <TableContainer component={Paper}>
-          <Table
-            sx={{
-              backgroundColor: "#030303e6",
-            }}
-            aria-label="simple table"
-          >
-            <TableHead>
-              <TableRow>
-                <div></div>
-                {Object.keys(monthNumbers).map((month) => {
-                  return (
-                    <StyledTableCell
-                      component="th"
-                      scope="row"
-                      size="small"
-                      padding="none"
-                      align="left"
-                      colSpan={monthAndWeekCount[month].weekCount}
-                    >
-                      {monthAndWeekCount[month].name}
-                    </StyledTableCell>
-                  );
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {Array.from(Array(7).keys()).map((row) => {
-                return (
-                  <TableRow key={row} sx={{}}>
-                    {getDayOfWeekRender(row)}
-                    {Array.from(Array(weekWithActivity.length).keys()).map(
-                      (col) => {
-                        if (
-                          weekWithActivity[col] !== undefined &&
-                          weekWithActivity[col][row] !== undefined
-                        ) {
-                          const dna = weekWithActivity[col][row];
-                          return (
-                            <StyledTableCell
-                              component="th"
-                              scope="row"
-                              size="small"
-                              padding="none"
-                            >
-                              <ActivityBox
-                                maxIntensity={maxIntensity}
-                                activity={dna.activity}
-                                date={dna.date}
-                              />
-                            </StyledTableCell>
-                          );
-                        } else {
-                          return (
-                            <StyledTableCell
-                              component="th"
-                              scope="row"
-                              size="small"
-                              padding="none"
-                            >
-                              <ActivityBox activity={[]} date={undefined} />
-                            </StyledTableCell>
-                          );
-                        }
-                      }
-                    )}
+      <>
+      <div className="lg:flex">
+          <div className="lg:flex justify-between py-2 md:px-3 border border-solid border-[#37415180] lg:w-[86.7%] rounded-lg bg-[#030303e6]">
+            <div className="flex flex-col w-[100%]">
+            <span className="text-2xl mr-4">
+              {totalGamesPlayed} games played in {selectedYear === currentDate ? 'the last year' : selectedYear}
+            </span>
+            <TableContainer component={Paper}>
+              <Table
+                sx={{
+                  backgroundColor: "#030303e6",
+                }}
+                aria-label="simple table"
+                >
+                <TableHead>
+                  <TableRow>
+                    <div></div>
+                    {Object.keys(monthNumbers).map((month) => {
+                      return (
+                        <StyledTableCell
+                        component="th"
+                        scope="row"
+                        size="small"
+                        padding="none"
+                        align="left"
+                        colSpan={monthAndWeekCount[month].weekCount}
+                        >
+                          {monthAndWeekCount[month].name}
+                        </StyledTableCell>
+                      );
+                    })}
                   </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+                </TableHead>
+                <TableBody>
+                  {Array.from(Array(7).keys()).map((row) => {
+                    return (
+                      <TableRow key={row} sx={{}}>
+                        {getDayOfWeekRender(row)}
+                        {Array.from(Array(weekWithActivity.length).keys()).map(
+                          (col) => {
+                            if (
+                              weekWithActivity[col] !== undefined &&
+                              weekWithActivity[col][row] !== undefined
+                            ) {
+                              const dna = weekWithActivity[col][row];
+                              return (
+                                <>
+                                  <StyledTableCell
+                                    component="th"
+                                    scope="row"
+                                    size="small"
+                                    padding="none"
+                                    >
+                                    <ActivityBox
+                                      maxIntensity={maxIntensity}
+                                      activity={dna.activity}
+                                      date={dna.date}
+                                      year={dna.date.getFullYear().toString()}
+                                      selectedYear={selectedYear}
+                                      currentDate={currentDate}
+                                      />
+                                  </StyledTableCell>
+                                </>
+                              );
+                            } else {
+                              return (
+                                <>
+                                  <StyledTableCell
+                                    component="th"
+                                    scope="row"
+                                    size="small"
+                                    padding="none"
+                                    >
+                                    <ActivityBox
+                                      maxIntensity={0}
+                                      activity={[]}
+                                      date={undefined}
+                                      year={0}
+                                      selectedYear={selectedYear}
+                                      currentDate={currentDate}
+                                      />
+                                  </StyledTableCell>
+                                </>
+                              );
+                            }
+                          }
+                        )}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </div>
+          <div className="flex lg:flex-col gap-1 lg:ml-[10px] mg:mt-[10px]">
+            {years.map((item) => (
+              <button
+              className={`pl-[12px] block w-[100px] p-[8px] text-[13px] text-left font-sans rounded-[6px] select-none ${
+                String(item) === String(selectedYear) || (selectedYear === currentDate && String(item) === years[0])
+                ? "bg-[#3f6ba3] text-white" 
+                : "hover:bg-gray-800"
+              }`}
+              onClick={() => setSelectedYear(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+      </>
     );
-  }
 };
 
 export default ActivityDiagram;
