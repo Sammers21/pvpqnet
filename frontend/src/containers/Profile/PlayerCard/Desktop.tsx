4af7b6338b6fa6f60c13d8710fe6ae8d5d15e717
Artyom Bolyachinov <artembolyachinov@gmail.com>
2025-08-31 22:34:01 +0300

    feature: preloader for avatar at profile page

--- frontend/src/containers/Profile/PlayerCard/Desktop.tsx
+++ frontend/src/containers/Profile/PlayerCard/Desktop.tsx
@@ -2,25 +2,51 @@ import { openWowArmory } from "@/utils/urlparts";
 import { PlayerItemLvl, PlayerImages, PlayerName } from "./common";
 
 import type { Player } from "@/types";
+import { CircularProgress } from "@mui/material";
+import { useState, useEffect } from "react";
 
 interface IProps {
   player: Player;
   openArmory?: () => void;
 }
 
-const PlayerImage = ({ player }: IProps) => (
-  <>
-    <div className="smoke-bg absolute left-0 top-0 w-full h-full bg-cover bg-no-repeat rounded-xl opacity-50" />
-
-    {player.media?.main_raw && (
-      <img
-        className="-scale-x-100 object-cover select-none"
-        alt="Player"
-        src={player.media.main_raw}
-      />
-    )}
-  </>
-);
+const PlayerImage = ({ player }: IProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  
+  useEffect(() => {
+    if (!player.media.main_raw) return;
+    const playerImage = new Image();
+    playerImage.src = player.media.main_raw;
+    playerImage.onload = () => {
+      setIsLoaded(true);
+    };
+  },[player])
+  if (!player.media.main_raw){
+    return (
+      <div>
+        <div className="smoke-bg absolute left-0 top-0 w-[300px] h-full bg-cover bg-no-repeat rounded-xl opacity-50" />
+        <div className="w-[300px] h-full flex justify-center items-center">
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div className="smoke-bg absolute left-0 top-0 w-[300px] h-full bg-cover bg-no-repeat rounded-xl opacity-50" />
+      {player.media?.main_raw && isLoaded ? (
+        <img
+          className="-scale-x-100 w-full h-full object-cover select-none"
+          alt="Player"
+          src={player.media.main_raw}
+        />
+      ) : (
+        <div className="w-[300px] h-full flex justify-center items-center">
+          <CircularProgress></CircularProgress>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const PlayerCard = ({ player }: IProps) => {
   const openArmory = () => {
